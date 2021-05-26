9d35c803f4363fee8f1c144385c4c3add2a57426
Yakov Gurevich <umanrh@gmail.com>
2021-05-26 15:41:29 +0300

    some updates

--- client/src/components/Container.component.js
+++ client/src/components/Container.component.js
@@ -6,13 +6,27 @@ const Container = ({ community }) => {
     <div className="containerWrap">
       <div className="container">
         <h1>{community.name}</h1>
-        <h2>{community.owner.city}</h2>
+        <h2>
+          <i className="fas fa-map-marker-alt"></i>
+          <span> </span>
+          {community.owner.city}
+        </h2>
 
         <p>
+          <i className="fas fa-location-arrow"></i>
+          <span> </span>
           {community.address.street}, {community.address.city}
         </p>
-        <p>{community.owner.name}</p>
-        <p>{community.owner.phone}</p>
+        <p>
+          <i className="fas fa-male"></i>
+          <span className="space"> </span>
+          {community.owner.name}
+        </p>
+        <p>
+          <i className="fas fa-address-book"></i>
+          <span className="space"> </span>
+          {community.owner.phone}
+        </p>
       </div>
       <button>
         <Link to={`/community/${community.name}`}>Подробнее</Link>
